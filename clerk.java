2a156400b6bcacc4fa8cd874976f93e855f5810f
Yusako <Yusako@users.noreply.github.com>
2024-03-08 18:50:48 +0100

    simplified sse approach

--- clerk.java
+++ clerk.java
@@ -6,11 +6,7 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Locale;
-import java.util.Map;
-import java.util.Optional;
 import java.util.Random;
-import java.util.Set;
 import java.util.stream.*;
 import java.io.FileInputStream;
 import java.io.InputStream;
@@ -23,46 +19,133 @@ import com.sun.net.httpserver.HttpExchange;
 
 // jshell -R-ea --enable-preview
 
-interface ClerkOutput {
+class Clerk {
+    static String templateFileName = "./web/indexTemplate.html";
+    static String preContent = "",
+                  content = "",
+                  postContent = "";
 
-    String getTarget();
+    static LiveView view;
 
-    void markdown(String markdown, String id);
+    static void writeToFile(String fileName, String text) {
+        try {
+            Files.writeString(Path.of(fileName), text);
+        } catch (IOException e) {
+            System.err.printf("Error writing %s\n", e.getMessage());
+            System.exit(1);
+        }
+    }
+    static void writeResult(String fileName) {
+        writeToFile(fileName, STR.
+            """
+            \{preContent}
+            \{content}
+            \{postContent}
+            """);
+    }
 
-    void script(String addonIdentifier, String code, Object... args);
+    static String cutOut(String fileName, boolean includeStartLabel, boolean includeEndLabel, String... labels) {
+        List<String> snippet = new ArrayList<>();
+        boolean skipLines = true;
+        boolean isInLabels;
+        try {
+            List<String> lines = Files.readAllLines(Path.of(fileName));
+            for (String line : lines) {
+                isInLabels = Arrays.stream(labels).anyMatch(label -> line.trim().equals(label));
+                if (isInLabels) {
+                    if (skipLines && includeStartLabel)
+                        snippet.add(line);
+                    if (!skipLines && includeEndLabel)
+                        snippet.add(line);
+                    skipLines = !skipLines;
+                    continue;
+                }
+                if (skipLines)
+                    continue;
+                snippet.add(line);
+            }
+        } catch (IOException e) {
+            System.err.printf("Error reading %s\n", e.getMessage());
+            System.exit(1);
+        }
+        return snippet.stream().collect(Collectors.joining("\n")) + "\n";
+    }
 
-    void write(String text);
+    static String cutOut(String fileName, String... labels) {
+        return cutOut(fileName, false, false, labels);
+    }
 
-    void reset();
+    static String readFile(String fileName) {
+        return cutOut(fileName, true, true, "");
+    }
+
+    static String generateID(int n) {
+        String characters = "abcdefghijklmnopqrstuvwxyz0123456789";
+        Random rand = new Random();
+        return IntStream.rangeClosed(1, n)
+                .mapToObj(i -> "" + characters.charAt(rand.nextInt(characters.length())))
+                .collect(Collectors.joining());
+    }
+
+    // Open Server at default port
+    static void serve() {
+        serve(LiveView.defaultPort);
+    }
+
+    // Open Server at custom port
+    static void serve(int port) {
+        if (view != null) {
+            view.stop();
+        }
+
+        try {
+            view = LiveView.of(port);
+        } catch (Exception e) {
+            System.err.printf("Error starting Server: %s\n", e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    // Send HTML Elements
+    static void write(String text) {
+        view.send(STR."write:\{text}");
+    }
+
+    // Send Javascript code
+    static void script(String code) {
+        // bind declarations to this to make them accessible between calls
+        String evalCode = code
+                .replaceAll("(var |const |let )", "this.")
+                .replaceAll("function\\s+(\\S+)\\s*\\((.*)\\)\\s*\\{", "this.$1 = ($2) => {");
+
+        view.send(STR."script:\{evalCode}");
+    }
+
+    // Markdown as example on how to use write() and script()
+    static void markdown(String markdown) {
+        String ID = generateID(10);
+        write(STR."""
+            <div id="\{ID}">
+            \{markdown}
+            </div>
+                """);
+        script(STR."""
+            var markdownContent = document.getElementById("\{ID}").textContent;
+            var renderedHTML = marked.parse(markdownContent);
+            document.getElementById("\{ID}").innerHTML = renderedHTML;
+            """);
+    }
 }
 
-abstract class ClerkAddonOutput {
-    static Set<ClerkAddon> addons;
-}
-
-class ClerkServer extends ClerkAddonOutput implements ClerkOutput {
+class LiveView {
     static int defaultPort = 50001;
     private static final Map<String, String> mimeTypes = new HashMap<>();
 
     final HttpServer server;
-    final String index = "./web/server/index.html";
+    final String index = "./web/index.html";
     List<HttpExchange> activeConnections;
 
     int port;
-
-    private enum Commands {
-        Markdown,
-        Script,
-        Write
-    }
-
-    private record Message(Commands cmd, String addonId, String content) {
-        @Override
-        public String toString() {
-            return "{\"command\": \"" + cmd + "\", \"addonId\": \"" + addonId + "\", \"content\": \""
-                    + content.replaceAll("(\")+", "\\\\\"") + "\"}";
-        }
-    }
 
     static {
         mimeTypes.put("html", "text/html");
@@ -70,15 +153,15 @@ class ClerkServer extends ClerkAddonOutput implements ClerkOutput {
         mimeTypes.put("css", "text/css");
     }
 
-    public static ClerkServer of() throws IOException {
+    public static LiveView of() throws IOException {
         return of(defaultPort);
     }
 
-    public static ClerkServer of(int port) throws IOException {
-        return new ClerkServer(port);
+    public static LiveView of(int port) throws IOException {
+        return new LiveView(port);
     }
 
-    public ClerkServer(int port) throws IOException {
+    public LiveView(int port) throws IOException {
         this.port = port;
         activeConnections = new ArrayList<>();
 
@@ -133,8 +216,9 @@ class ClerkServer extends ClerkAddonOutput implements ClerkOutput {
         }
     }
 
+    // event = "action:data"
     void send(String event) {
-        List<HttpExchange> dead = new ArrayList<>();
+        List<HttpExchange> deadConnections = new ArrayList<>();
 
         for (HttpExchange exchange : activeConnections) {
             try {
@@ -143,341 +227,64 @@ class ClerkServer extends ClerkAddonOutput implements ClerkOutput {
                                 .getBytes(StandardCharsets.UTF_8));
                 exchange.getResponseBody().flush();
             } catch (Exception e) {
-                dead.add(exchange);
+                deadConnections.add(exchange);
             }
         }
-        activeConnections.removeAll(dead);
+        activeConnections.removeAll(deadConnections);
     }
 
-    @Override
-    public String getTarget() {
-        return Integer.toString(port);
-    }
-
-    @Override
-    public void markdown(String markdown, String unused) {
-        send(new Message(Commands.Markdown, "", markdown).toString());
-
-    }
-
-    @Override
-    public void script(String addonIdentifier, String code, Object... args) {
-        Optional<ClerkAddon> addon = addons.stream().filter(x -> x.getIdentifier().equals(addonIdentifier)).findFirst();
-        if (addon.isEmpty())
-            return;
-
-        send(new Message(Commands.Script, addonIdentifier,
-                String.format(Locale.ENGLISH, addon.get().translateToServeStatements(code), args))
-                .toString());
-
-    }
-
-    @Override
-    public void write(String text) {
-        send(new Message(Commands.Write, "", text).toString());
-
-    }
-
-    @Override
-    public void reset() {
+    public void stop() {
         server.stop(0);
     }
 }
 
-class ClerkFileOutput extends ClerkAddonOutput implements ClerkOutput {
-    static String defaultLocation = "./build/index.html";
-    String preContent = "",
-            postContent = "",
-            content = "";
-    String outputLocation;
-
-    static ClerkFileOutput of(String preContent, String postContent) {
-        return of(defaultLocation, preContent, postContent);
-    }
-
-    static ClerkFileOutput of(String outputLocation, String preContent, String postContent) {
-        ClerkFileOutput cfo = new ClerkFileOutput();
-        cfo.outputLocation = outputLocation;
-        cfo.preContent = preContent;
-        cfo.postContent = postContent;
-        cfo.writeResult(outputLocation);
-        return cfo;
-    }
-
-    void writeToFile(String fileName, String text) {
-        try {
-            Files.createDirectories(Path.of(fileName).getParent());
-            Files.writeString(Path.of(fileName), text);
-        } catch (IOException e) {
-            System.err.printf("Error writing %s\n", e.getMessage());
-            System.exit(1);
-        }
-    }
-
-    void writeResult(String fileName) {
-        writeToFile(fileName, STR."""
-            \{preContent}
-            \{content}
-            \{postContent}
-            """);
-    }
-
-    @Override
-    public void write(String text) {
-        content = STR."""
-        \{content}
-        \{text}
-        """;
-        writeResult(outputLocation);
-    }
-
-    void write(Object obj) {
-        write(STR."<code><pre>\{obj}</pre></code>");
-    }
-
-    @Override
-    public void markdown(String markdown, String id) {
-        write(STR."""
-            <div id="\{id}">
-            \{markdown}
-            </div>
-            <script>
-                var markdownContent = document.getElementById("\{id}").textContent;
-                var renderedHTML = marked.parse(markdownContent);
-                document.getElementById("\{id}").innerHTML = renderedHTML;
-            </script>
-            """);
-    }
-
-    @Override
-    public void script(String addonIdentifier, String code, Object... args) {
-        Optional<ClerkAddon> addon = addons.stream().filter(x -> x.getIdentifier().equals(addonIdentifier)).findFirst();
-        if (addon.isEmpty())
-            return;
-
-        write(String.format(Locale.ENGLISH, addon.get().translateToBuildStatements(code), args));
-    }
-
-    @Override
-    public String getTarget() {
-        return outputLocation;
-    }
-
-    @Override
-    public void reset() {
-        content = "";
-    }
-}
-
-class Clerk {
-    static String templateFileName = "./web/indexTemplate.html";
-    static List<ClerkOutput> outputs = new ArrayList<>();
-
-    static {
-        ClerkAddonOutput.addons = Set.of(new Turtle());
-    }
-
-    static String cutOut(String fileName, boolean includeStartLabel, boolean includeEndLabel, String... labels) {
-        List<String> snippet = new ArrayList<>();
-        boolean skipLines = true;
-        boolean isInLabels;
-        try {
-            List<String> lines = Files.readAllLines(Path.of(fileName));
-            for (String line : lines) {
-                isInLabels = Arrays.stream(labels).anyMatch(label -> line.trim().equals(label));
-                if (isInLabels) {
-                    if (skipLines && includeStartLabel)
-                        snippet.add(line);
-                    if (!skipLines && includeEndLabel)
-                        snippet.add(line);
-                    skipLines = !skipLines;
-                    continue;
-                }
-                if (skipLines)
-                    continue;
-                snippet.add(line);
-            }
-        } catch (IOException e) {
-            System.err.printf("Error reading %s\n", e.getMessage());
-            System.exit(1);
-        }
-        return snippet.stream().collect(Collectors.joining("\n")) + "\n";
-    }
-
-    static String cutOut(String fileName, String... labels) {
-        return cutOut(fileName, false, false, labels);
-    }
-
-    static String readFile(String fileName) {
-        return cutOut(fileName, true, true, "");
-    }
-
-    static String generateID(int n) {
-        String characters = "abcdefghijklmnopqrstuvwxyz0123456789";
-        Random rand = new Random();
-        return IntStream.rangeClosed(1, n)
-                .mapToObj(i -> "" + characters.charAt(rand.nextInt(characters.length())))
-                .collect(Collectors.joining());
-    }
-
-    static void serve() {
-        serve(ClerkServer.defaultPort);
-    }
-
-    static void serve(int port) {
-        Optional<ClerkOutput> existingOutput = outputs.stream()
-                .filter(x -> x.getTarget().equals(Integer.toString(port)))
-                .findFirst();
-        if (existingOutput.isPresent()) {
-            existingOutput.get().reset();
-            outputs.remove(existingOutput.get());
-        }
-
-        try {
-            outputs.add(ClerkServer.of(port));
-        } catch (Exception e) {
-            System.err.printf("Error starting Server: %s\n", e.getMessage());
-            e.printStackTrace();
-        }
-    }
-
-    static void addFileOutput() {
-        addFileOutput(ClerkFileOutput.defaultLocation);
-    }
-
-    static void addFileOutput(String location) {
-        String preContent = cutOut(templateFileName, true, false, "<!DOCTYPE html>", "<!-- begin include content -->");
-        String postContent = cutOut(templateFileName, "<!-- end include content -->");
-
-        Optional<ClerkOutput> existingOutput = outputs.stream().filter(x -> x.getTarget().equals(location)).findFirst();
-        if (existingOutput.isPresent()) {
-            outputs.remove(existingOutput.get());
-        }
-        outputs.add(ClerkFileOutput.of(location, preContent, postContent));
-    }
-
-    static void write(String text) {
-        outputs.forEach(o -> o.write(text));
-    }
-
-    static void script(String addonIdentifier, String code, Object... args) {
-        outputs.forEach(o -> o.script(addonIdentifier, code, args));
-    }
-
-    static void markdown(String markdown) {
-        String ID = generateID(10);
-        outputs.forEach(o -> o.markdown(markdown, ID));
-    }
-}
-
-interface ClerkAddon {
-    default String getIdentifier() {
-        return this.getClass().getSimpleName();
-    }
-
-    String translateToBuildStatements(String statement);
-
-    String translateToServeStatements(String statement);
-}
-
-class Turtle implements ClerkAddon {
-    private static Map<String, String> toBuild, toServe;
+class Turtle {
     final String ID;
     final int width, height;
-
-    static {
-        populateBuildMap();
-        populateServeMap();
-    }
 
     Turtle(int width, int height) {
         this.width = width;
         this.height = height;
         ID = Clerk.generateID(6);
-
         Clerk.write(STR."""
-            <canvas id="turtleCanvas\{ID}" width="\{width}" height="\{height}" style="border:1px solid #000;"></canvas>
+        <canvas id="turtleCanvas\{ID}" width="\{width}" height="\{height}" style="border:1px solid #000;"></canvas>
         """);
-        Clerk.script(this.getClass().getSimpleName(), "init", ID, ID);
+        Clerk.script(STR."const turtle\{ID} = new Turtle(document.getElementById('turtleCanvas\{ID}'));");
     }
 
     Turtle() {
         this(500, 500);
     }
 
-    private static void populateServeMap() {
-        String init = STR."{\"command\": \"init\", \"id\": \"%s\", \"value\": \"%s\"}";
-        String penDown = STR."{\"command\": \"penDown\", \"id\": \"%s\", \"value\": \"0\"}";
-        String penUp = STR."{\"command\": \"penUp\", \"id\": \"%s\", \"value\": \"0\"}";
-        String forward = STR."{\"command\": \"forward\", \"id\": \"%s\", \"value\": \"%f\"}";
-        String backward = STR."{\"command\": \"backward\", \"id\": \"%s\", \"value\": \"%f\"}";
-        String left = STR."{\"command\": \"left\", \"id\": \"%s\", \"value\": \"%f\"}";
-        String right = STR."{\"command\": \"right\", \"id\": \"%s\", \"value\": \"%f\"}";
-
-        toServe = Map.of("init", init,
-                "penDown", penDown,
-                "penUp", penUp,
-                "forward", forward,
-                "backward", backward,
-                "left", left,
-                "right", right);
-    }
-
-    private static void populateBuildMap() {
-        String init = STR."<script>const turtle%s = new Turtle(document.getElementById('turtleCanvas%s'));</script>";
-        String penDown = STR."<script>turtle%s.penDown();</script>";
-        String penUp = STR."<script>turtle%s.penUp(); </script>";
-        String forward = STR."<script>turtle%s.forward(%f);</script>";
-        String backward = STR."<script>turtle%s.backward(%f);</script>";
-        String left = STR."<script>turtle%s.left(%f);</script>";
-        String right = STR."<script>turtle%s.right(%f);</script>";
-
-        toBuild = Map.of("init", init,
-                "penDown", penDown,
-                "penUp", penUp,
-                "forward", forward,
-                "backward", backward,
-                "left", left,
-                "right", right);
-    }
-
     Turtle penDown() {
-        Clerk.script(this.getClass().getSimpleName(), "penDown", ID);
+        Clerk.script(STR."turtle\{ID}.penDown();");
         return this;
     }
 
     Turtle penUp() {
-        Clerk.script(this.getClass().getSimpleName(), "penUp", ID);
+        Clerk.script(STR."turtle\{ID}.penUp();");
         return this;
     }
 
     Turtle forward(double distance) {
-        Clerk.script(this.getClass().getSimpleName(), "forward", ID, distance);
+        Clerk.script(STR."turtle\{ID}.forward(\{distance});");
         return this;
     }
 
     Turtle backward(double distance) {
-        Clerk.script(this.getClass().getSimpleName(), "backward", ID, distance);
+        Clerk.script(STR."turtle\{ID}.backward(\{distance});");
         return this;
     }
 
     Turtle left(double degrees) {
-        Clerk.script(this.getClass().getSimpleName(), "left", ID, degrees);
+        Clerk.script(STR."turtle\{ID}.left(\{degrees});");
         return this;
     }
 
     Turtle right(double degrees) {
-        Clerk.script(this.getClass().getSimpleName(), "right", ID, degrees);
+        Clerk.script(STR."turtle\{ID}.right(\{degrees});");
         return this;
     }
-
-    @Override
-    public String translateToBuildStatements(String statement) {
-        return toBuild.get(statement);
-    }
-
-    @Override
-    public String translateToServeStatements(String statement) {
-        return toServe.get(statement);
-    }
 }
+
+
---
+++ web/script.js
@@ -0,0 +1,42 @@
+function setUp() {    
+    if (window.EventSource) {
+        const source = new EventSource("/events");
+
+        source.onmessage = function (event) {
+            const splitPos = event.data.indexOf(':');
+            const action = event.data.slice(0, splitPos);
+            const data = event.data.slice(splitPos + 1).replaceAll("\\n", "\n");
+            
+            switch (action) {
+                case "script":
+                    script(data);
+                    break;
+                case "write":
+                    write(data);
+                    break;
+                default:
+                    console.log("Unknown Action");
+                    break;
+            }
+        };
+
+        source.onerror = function (error) {
+            console.error("EventSource failed:", error);
+            source.close();
+        };
+    } else {
+        document.getElementById("events").innerHTML = "Your browser does not support Server-Sent Events.";
+    }
+}
+
+function script(input) {
+    eval(input);
+}
+
+function write(input) {
+    const newElement = document.createElement("p");
+    newElement.innerHTML = input;
+    document.getElementById("events").appendChild(newElement);
+}
+
+setUp();
