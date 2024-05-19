3c7ed012fee43156c821fd5585ec3ed09f5f27c3
Dominikus Herzberg <denkspuren@users.noreply.github.com>
2024-05-19 17:02:27 +0200

    Syntax-Highlighting ergänzt

--- clerks/Markdown/MarkdownIt.java
+++ clerks/Markdown/MarkdownIt.java
@@ -5,8 +5,24 @@ record MarkdownIt(LiveView view) implements Clerk {
         String onlinePath = "https://cdn.jsdelivr.net/npm/markdown-it@14.1.0/dist/markdown-it.min.js";
         String localPath = "clerks/Markdown/markdown-it.min.js";
         Clerk.load(view, onlinePath, localPath);
+        Clerk.load(view, "clerks/Markdown/highlight.min.js");
+        // Clerk.script(view, STR."""
+        //     var md = markdownit({
+        //         html: true,
+        //         linkify: true,
+        //         typographer: true
+        //     });
+        //     """);
         Clerk.script(view, STR."""
             var md = markdownit({
+                highlight: function (str, lang) {
+                    if (lang && hljs.getLanguage(lang)) {
+                        try {
+                            return hljs.highlight(str, { language: lang }).value;
+                        } catch (__) {}
+                    }
+                    return ''; // use external default escaping
+                },
                 html: true,
                 linkify: true,
                 typographer: true
